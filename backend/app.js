e77e652d3f2eff5167939d84b5c62f16d3611152
Victor <zapatavictor186@gmail.com>
2025-04-11 19:21:19 -0300

    fix-cors-problem

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
+
 app.get('/', (req, res) => {
   res.send('¡Hola mundo desde Express!');
 });
